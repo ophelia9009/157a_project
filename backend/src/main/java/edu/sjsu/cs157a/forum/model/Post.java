2faef6ad0913d4ed8e529f5bd5dab63dbaf5a575
Yeqing Xu <yeqing.xu@sjsu.edu>
2025-05-03 15:58:51 -0700

    Add Post model

---
+++ backend/src/main/java/edu/sjsu/cs157a/forum/model/Post.java
@@ -0,0 +1,70 @@
+package edu.sjsu.cs157a.forum.model;
+
+import java.util.Date;
+
+public class Post {
+    private Integer id;
+    private String title;
+    private String content;
+    private User author;
+    private Subforum subforum;
+    private Date createdAt;
+    private Date updatedAt;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Subforum getSubforum() {
+        return subforum;
+    }
+
+    public void setSubforum(Subforum subforum) {
+        this.subforum = subforum;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
